d64f5f1da33004c36d61abc3003f4e71727c7857
userman123 <dominic.freeman123@gmail.com>
2019-10-15 14:53:07 +0100

    Add some more to popup

--- app/controllers/viability_appraisals_controller.rb
+++ app/controllers/viability_appraisals_controller.rb
@@ -1,6 +1,6 @@
 class ViabilityAppraisalsController < ApplicationController
   def index
     @viability_appraisals = ViabilityAppraisal.all
-    @viability_appraisal_markers = ViabilityAppraisal.where.not(latitude: nil, longitude: nil).map{ |va| {:latlng => [va.latitude, va.longitude], :popup => va.name} }
+    @viability_appraisal_markers = ViabilityAppraisal.where.not(latitude: nil, longitude: nil).map{ |va| {:latlng => [va.latitude, va.longitude], :popup => va.popup} }
   end
 end
--- app/models/viability_appraisal.rb
+++ app/models/viability_appraisal.rb
@@ -1,2 +1,8 @@
 class ViabilityAppraisal < ApplicationRecord
+  def popup
+    "<strong>#{name}</strong>
+     <p>Residential units: #{residential_units}</p>
+     <p>Habitable rooms: #{habitable_rooms}</p>
+     <p>Submission date: #{date_submitted}</p>"
+  end
 end
