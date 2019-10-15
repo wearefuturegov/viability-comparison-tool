3edbaddc1a79a00e2138c2d947abd0777213f611
userman123 <dominic.freeman123@gmail.com>
2019-10-15 12:52:13 +0100

    Add map

--- app/controllers/viability_appraisals_controller.rb
+++ app/controllers/viability_appraisals_controller.rb
@@ -1,5 +1,6 @@
 class ViabilityAppraisalsController < ApplicationController
   def index
     @viability_appraisals = ViabilityAppraisal.all
+    @viability_appraisal_markers = ViabilityAppraisal.where.not(latitude: nil, longitude: nil).map{ |va| {:latlng => [va.latitude, va.longitude], :popup => va.name} }
   end
 end
